867588d49765dee7214790a94291df9954196e35
Williamli1127 <stevenlee1127@gmail.com>
2021-04-06 16:12:17 +0800

    4/6

--- WastonAndAbili.cpp
+++ WastonAndAbili.cpp
@@ -1,101 +1,114 @@
 #include "stdio.h"
 #include "string.h"
 #include "stdlib.h"
+#include "stdint.h"
 
-typedef struct Node{
-  int value;
-  struct Node *next;
-}node;
 
-typedef Node *Stack;
-Stack top = NULL;
+//Ref
+// https://zh.wikipedia.org/wiki/%E5%BC%82%E6%88%96%E9%93%BE%E8%A1%A8
+// https://www.techiedelight.com/xor-linked-list-overview-implementation-c-cpp/
 
-int isEmpty(){
-  if(top==NULL){
+struct Node
+{
+    int value;
+    struct Node* link;
+};
+typedef struct Node* track;
+
+track XOR(track x, track y) {
+    return (track)((uintptr_t)(x) ^ (uintptr_t)(y));
+}
+
+void show_track(track whichone){
+  if(whichone == NULL){printf("\n");return;}
+  track current = whichone;
+  track previous = NULL;
+  track next;
+
+  while(current!=NULL){
+    printf("%d ",current->value);
+    next = XOR(previous,current->link);
+    previous = current;
+    current = next;
+  }
+  printf("\n");
+}
+
+void enter(Node** which_train,int label){
+  track new_node = (track)malloc(sizeof(Node));
+  new_node ->value = label;
+  new_node ->link = *which_train;
+
+  if(*which_train!=NULL){
+    (*which_train)->link = XOR(new_node,(*which_train)->link);
+  }
+
+  *which_train = new_node;
+}
+
+int leave(Node** which_train){
+  if((*which_train)==NULL){return 1;}
+  if(!(*which_train)){
     return 1;
   }
-  else{
-    return 0;
+  int data = (*which_train)->value;
+  track temp = (*which_train)->link;
+  if(temp){
+    temp->link = XOR((*which_train),temp->link);
+  }
+  free(*which_train);
+  *which_train = temp;
+  return data;
+}
+
+int isemptytrack(Node** which_train){
+  if((*which_train)==NULL){return 0;}
+  else{return 1;}
+}
+void migrate(Node** A_train,Node** B_train){
+  while(isemptytrack(A_train)){
+    enter(B_train,leave(A_train));
   }
 }
 
-void push(char data){
-  Stack new_node;
+void output(Node** which_train){
+  track *another_track = (track*)malloc(sizeof(track));
+  another_track[0] = NULL;
 
-  new_node = (Stack)malloc(sizeof(Node));
-  new_node->value = data;
-  new_node->next = top;
-  top = new_node;
+  while(isemptytrack(which_train)){
+    enter(another_track,leave(which_train));
+  }
+  *which_train = *another_track;
+  free(another_track);
 }
 
-char pop(Stack WhichRail){
-  printf("\nNow\n");
-  Stack ptr;
-  char temp;
-  printf("\nNow we pop : \n");
-  ptr = WhichRail;
-  temp = ptr->value;
-  top = top->next;
-  printf("%c",temp);
-  free(ptr);
-  return temp;
-
-}
-
-void Enter(int line,int label,Stack **rails){
-  Stack new_node;
-  new_node = (Stack)malloc(sizeof(Node));
-  new_node->value = label;
-  new_node->next = rails[line];
-  rails[line] = new_node;
-
-  //test pop
-  // Stack ptr;
-  // int temp;
-  // while(rails[line]!=NULL){
-  //   printf("\ntest!!\n");
-  //   ptr = rails[line];
-  //   temp = ptr->value;
-  //   rails[line] = rails[line]->next;
-  //   printf("--%d\n",temp);
-  // }
-  // free(ptr);
 
 
-  // rails[line]->value = label;
-  // rails[line] = rails[line]->next;
-}
 
 int main(){
   int k,n,r,l,r_a,r_b;
   char input[10];
   scanf("%d %d",&k,&n);
-
-  Stack *rails = (Stack*)malloc(sizeof(Stack)*k);
+  track *rails = (track*)malloc(sizeof(track)*k);
   for(int i=0;i<k;i++){rails[i] = NULL;}
-
 
   for(int i=0;i<n;i++){
     scanf("%s",input);
-
     if(strcmp(input,"enter") == 0){
-      printf("ENTER : \n");
       scanf("%d %d",&r,&l);
-      Enter(r,l,&rails);
-      pop(rails[r]);
+      enter(&rails[r],l);
     }
     else if(strcmp(input,"leave") == 0){
-      printf("LEAVE :　\n");
       scanf("%d",&r);
+      leave(&rails[r]);
+      // printf("\n~~%d\n",leave(&rails[r]));
     }
     else if(strcmp(input,"migrate") == 0){
-      printf("MIGRATE : \n");
       scanf("%d %d",&r_a,&r_b);
+      migrate(&rails[r_a],&rails[r_b]);
     }
-
   }
-
-
-
+  for(int i=0;i<k;i++){output(&rails[i]);}
+  for(int i=0;i<k;i++){show_track(rails[i]);}
   return 0;
 }
